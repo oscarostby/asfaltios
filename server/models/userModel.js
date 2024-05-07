fc73b756848fae748e646b6e21477f8c167644be
Maamua06 <115535704+Maamua06@users.noreply.github.com>
2024-05-07 12:13:11 +0200

    sdjjddj

---
+++ server/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('users', userSchema);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 const express = require('express');
 require('dotenv').config();
 
+// Imports
+const DB = require('./handlers/mongodb');
+
 const app = express();
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT, DB)
+
+// Middelware
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next();
+});
 
